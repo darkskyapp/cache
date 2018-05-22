d3616ed139790b6d30e47a23ce2e004742ec2dda
J. T. L <ironwallaby@gmail.com>
2018-05-22 11:28:37 -0400

    Switch tests out to async

--- test.js
+++ test.js
@@ -27,108 +27,96 @@ class TestCache extends Cache {
     return 10;
   }
 
-  promise() {
+  async promise() {
     this._y++;
-    return Bluebird.resolve(this._y);
+    return this._y;
   }
 }
 
 class FailCache extends TestCache {
-  promise() {
+  async promise() {
     this._y++;
-    return Bluebird.reject(new Error("sad"));
+    throw new Error("sad");
   }
 }
 
 describe("cache", function() {
-  it("should only call the backing function once serially", () => {
+  it("should only call the backing function once serially", async () => {
     const test = new TestCache();
+    expect(await test.get()).to.equal(1);
+    expect(test.get_calls()).to.equal(1);
+    expect(test.promise_calls()).to.equal(1);
 
-    return test.get().
-      then(count => {
-        expect(test.get_calls()).to.equal(1);
-        expect(test.promise_calls()).to.equal(1);
-        expect(count).to.equal(1);
+    expect(await test.get()).to.equal(1);
+    expect(test.get_calls()).to.equal(2);
+    expect(test.promise_calls()).to.equal(1);
 
-        return test.get().
-          then(count => {
-            expect(test.get_calls()).to.equal(2);
-            expect(test.promise_calls()).to.equal(1);
-            expect(count).to.equal(1);
-
-            return test.get().
-              then(count => {
-                expect(test.get_calls()).to.equal(3);
-                expect(test.promise_calls()).to.equal(1);
-                expect(count).to.equal(1);
-              });
-          });
-      });
+    expect(await test.get()).to.equal(1);
+    expect(test.get_calls()).to.equal(3);
+    expect(test.promise_calls()).to.equal(1);
   });
 
-  it("should only call the backing function once in parallel", () => {
+  it("should only call the backing function once in parallel", async () => {
     const test = new TestCache();
 
-    return Bluebird.
-      all([test.get(), test.get(), test.get()]).
-      then(counts => {
-        expect(test.get_calls()).to.equal(3);
-        expect(test.promise_calls()).to.equal(1);
-        expect(counts).to.deep.equal([1, 1, 1]);
-      });
+    expect(await Bluebird.all([test.get(), test.get(), test.get()])).
+      to.deep.equal([1, 1, 1]);
+    expect(test.get_calls()).to.equal(3);
+    expect(test.promise_calls()).to.equal(1);
   });
 
-  it("should call the backing function again on a timeout", () => {
+  it("should call the backing function again on a timeout", async () => {
     const test = new TestCache();
-
-    return Bluebird.
-      all([
-        Bluebird.delay( 0).bind(test).then(test.get), // uncached
-        Bluebird.delay( 0).bind(test).then(test.get), // cached
-        Bluebird.delay(20).bind(test).then(test.get), // uncached
-        Bluebird.delay(20).bind(test).then(test.get), // cached
-        Bluebird.delay(20).bind(test).then(test.get), // cached
-      ]).
-      then(counts => {
-        expect(test.get_calls()).to.equal(5);
-        expect(test.promise_calls()).to.equal(2);
-        expect(counts).to.deep.equal([1, 1, 2, 2, 2]);
-      });
+    const counts = await Bluebird.all([
+      Bluebird.delay(0).bind(test).then(test.get), // uncached
+      Bluebird.delay(0).bind(test).then(test.get), // cached
+      Bluebird.delay(20).bind(test).then(test.get), // uncached
+      Bluebird.delay(20).bind(test).then(test.get), // cached
+      Bluebird.delay(20).bind(test).then(test.get), // cached
+    ]);
+    expect(counts).to.deep.equal([1, 1, 2, 2, 2]);
+    expect(test.get_calls()).to.equal(5);
+    expect(test.promise_calls()).to.equal(2);
   });
 
-  it("should not cache failures", () => {
+  it("should not cache failures", async () => {
     const test = new FailCache();
 
-    /* called serially, we hit the backing function each time since there's no
-     * caching on failure */
-    return test.get().
-      throw(new Error("promise should not resolve")).
-      catch(err => {
-        expect(test.get_calls()).to.equal(1);
-        expect(test.promise_calls()).to.equal(1);
-        expect(err).to.be.an.instanceof(Error).
-          with.property("message", "sad");
+    // called serially, we hit the backing function each time since there's no
+    // caching on failure
+    try {
+      await test.get();
+      throw new Error("promise should not resolve");
+    }
+    catch(err) {
+      expect(err).to.be.an.instanceof(Error).with.property("message", "sad");
+      expect(test.get_calls()).to.equal(1);
+      expect(test.promise_calls()).to.equal(1);
+      await Bluebird.delay(1);
 
-        return test.get().
-          throw(new Error("promise should not resolve")).
-          catch(err => {
-            expect(test.get_calls()).to.equal(2);
-            expect(test.promise_calls()).to.equal(2);
-            expect(err).to.be.an.instanceof(Error).
-              with.property("message", "sad");
+      try {
+        await test.get();
+        throw new Error("promise should not resolve");
+      }
+      catch(err) {
+        expect(err).to.be.an.instanceof(Error).with.property("message", "sad");
+        expect(test.get_calls()).to.equal(2);
+        expect(test.promise_calls()).to.equal(2);
+        await Bluebird.delay(1);
 
-            /* but when called in parallel, it's only hit once because it takes
-             * a while to get a response! */
-            return Bluebird.
-              all([test.get(), test.get()]).
-              throw(new Error("promise should not resolve")).
-              catch(err => {
-                expect(test.get_calls()).to.equal(4);
-                expect(test.promise_calls()).to.equal(3);
-                expect(err).to.be.an.instanceof(Error).
-                  with.property("message", "sad");
-              });
-          });
-      });
+        // but when called in parallel, it's only hit once because it takes
+        // a while to get a response!
+        try {
+          await Bluebird.all([test.get(), test.get()]);
+          throw new Error("promise should not resolve");
+        }
+        catch(err) {
+          expect(err).to.be.an.instanceof(Error).
+            with.property("message", "sad");
+          expect(test.get_calls()).to.equal(4);
+          expect(test.promise_calls()).to.equal(3);
+        }
+      }
+    }
   });
 });
